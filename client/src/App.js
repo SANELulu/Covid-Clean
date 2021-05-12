3c913e9b92dfa47e129b861eb9c7693307c5f179
Lucas Diaz <lucasdiaz22b@gmail.com>
2021-05-11 20:38:07 -0400

    basic components

--- client/src/App.js
+++ client/src/App.js
@@ -1,21 +1,16 @@
-import React, { Component } from "react";
-import logo from "./logo.svg";
-import "./App.css";
-
-class App extends Component {
-  render() {
-    return (
-      <div className="App">
-        <div className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h2>Welcome to React</h2>
-        </div>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
-      </div>
-    );
-  }
+import React from "react";
+import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
+import Nav from "./components/Nav";
+import Login from "./components/Login";
+function App() {
+  return (
+    <Router>
+      <Nav />
+      <Switch>
+        <Route exact path="/login" component={Login} />
+      </Switch>
+    </Router>
+  );
 }
 
 export default App;
---
+++ client/src/components/Nav/index.js
@@ -0,0 +1,17 @@
+import React from "react";
+
+function Nav() {
+  return (
+    <nav className="navbar navbar-light bg-light">
+      <a className="navbar-brand" href="/">
+        Navbar
+      </a>
+
+      <a className="navbar-brand" href="/login">
+        LOGIN
+      </a>
+    </nav>
+  );
+}
+
+export default Nav;
