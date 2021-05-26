eb13b173f432d3adc5398dacf02e26d4e9807020
Lucas Diaz <lucasdiaz22b@gmail.com>
2021-05-25 20:49:23 -0400

    ok no broken no 'mo

--- client/src/pages/Locations.js
+++ client/src/pages/Locations.js
@@ -14,7 +14,7 @@ function Locations() {
     spacing: 8,
     root: {
       flexGrow: 1,
-      background: "linear-gradient(45deg, #FE6B8B 30%, #FF8E53 90%)",
+      // background: "linear-gradient(45deg, #FE6B8B 30%, #FF8E53 90%)",
     },
     image: {
       // width: "33%",
