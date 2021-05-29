d867dfadfb2376540e24d23691f957814e7c699f
Lucas Diaz <lucasdiaz22b@gmail.com>
2021-05-29 11:34:09 -0400

    heroku

--- server.js
+++ server.js
@@ -20,9 +20,9 @@ mongoose.connect("mongodb://localhost/covid");
 
 // Send every request to the React app
 // Define any API routes before this runs
-// app.get("*", function(req, res) {
-//   res.sendFile(path.join(__dirname, "./client/build/index.html"));
-// });
+app.get("*", function (req, res) {
+  res.sendFile(path.join(__dirname, "./client/build/index.html"));
+});
 
 app.listen(PORT, function () {
   console.log(`🌎 ==> API server now on port ${PORT}!`);
