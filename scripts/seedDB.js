64b763ae682e728866cc53cd2645211f66283160
Lucas Diaz <lucasdiaz22b@gmail.com>
2021-05-29 13:09:13 -0400

    heroku

--- scripts/seedDB.js
+++ scripts/seedDB.js
@@ -1,9 +1,9 @@
-const mongoose = require("mongoose");
+// const mongoose = require("mongoose");
 const db = require("../models");
 
 // This file empties the Posts collection and inserts the books below
 
-mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/covid");
+// mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/covid");
 
 const mapSeed = [
   {
